b3b6423fcf3c0217c0ee228157b0259bc6ccbd24
Topias Ukkonen <ukkonentopias@gmail.com>
2024-07-01 18:28:21 +0300

    small fix

--- insecure_project/urls.py
+++ insecure_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('system-info/', views.system_info, name='system_info')
+    path('system-info/', views.system_info, name='system_info'),
     path('logout/', views.user_logout, name='logout'),
     path('notes/', views.notes, name='notes'),
 ]
